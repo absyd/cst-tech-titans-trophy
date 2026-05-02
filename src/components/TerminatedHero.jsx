4f0f65e9ebd3986860e084b35b4c21d08e23d0f0
absyd <it.is.the.black.mamba@gmail.com>
2026-05-02 09:59:46 +0600

    match termination notice finalized

--- src/components/TerminatedHero.jsx
+++ src/components/TerminatedHero.jsx
@@ -5,9 +5,9 @@ const TerminatedHero = ({ timeLeft }) => {
     <div className='flex flex-col'>
     <div className="text-center">
       <h1 className='text-2xl font-bold text-yellow-300 mb-2'>Match Termination Notice</h1> 
-      <img src="/rain_termination.jpg" className='h-lg w-lg md:h-3xl md:w-3xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
+      <img src="/rain_termination.jpg" className='h-lg w-lg   md:w-xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
     </div>
-      <div className="my-2 md:w-3xl w-3xl mx-auto">
+      <div className="my-2 md:w-xl w-full mx-auto">
       <Countdown timeLeft={timeLeft} />
     </div>
     
--- src/pages/LiveScore.jsx
+++ src/pages/LiveScore.jsx
@@ -4,9 +4,13 @@ import TerminatedHero from "../components/TerminatedHero";
 export default function LiveScorePage() {
     return (
         <div className="min-h-screen">
-            <div className="pt-12">
-                {/* <LiveScore /> */}
-                <TerminatedHero/>
+            <div className='flex flex-col'>
+                <div className="text-center">
+                    <h1 className='text-2xl font-bold text-yellow-300 mb-2'>Match Termination Notice</h1>
+                    <img src="/rain_termination.jpg" className='h-lg w-lg   md:w-xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
+                </div>
+
+
             </div>
         </div>
     );
