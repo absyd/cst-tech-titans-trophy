ac761b20954b004c0e0c99c5b65747321ed43870
absyd <it.is.the.black.mamba@gmail.com>
2026-05-02 08:51:54 +0600

    termination responsive ness added

--- src/components/TerminatedHero.jsx
+++ src/components/TerminatedHero.jsx
@@ -4,7 +4,7 @@ const TerminatedHero = () => {
   return (
     <div>
       <h1 className='text-2xl font-bold text-yellow-300 mb-2'>Match Termination Notice</h1> 
-      <img src="/rain_termination.png" className='h-3xl w-3xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
+      <img src="/rain_termination.png" className='h-xl w-xl md:h-3xl md:w-3xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
     </div>
   )
 }
