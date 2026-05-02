41d8e3cd187c9693285c0a843a65e04f4f4040a4
absyd <it.is.the.black.mamba@gmail.com>
2026-05-02 09:08:47 +0600

    match update

--- src/components/TerminatedHero.jsx
+++ src/components/TerminatedHero.jsx
@@ -2,14 +2,15 @@ import Countdown from './Countdown'
 
 const TerminatedHero = ({ timeLeft }) => {
   return (
-    <div>
+    <div className='flex flex-col'>
     <div className="text-center">
       <h1 className='text-2xl font-bold text-yellow-300 mb-2'>Match Termination Notice</h1> 
-      <img src="/rain_termination.jpg" className='h-xl w-xl md:h-3xl md:w-3xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
+      <img src="/rain_termination.jpg" className='h-lg w-lg md:h-3xl md:w-3xl mx-auto rounded-lg border-2 border-yellow-300' alt="Rain Termination" />
     </div>
-    <div className="my-2">
+      <div className="my-2 md:w-3xl w-3xl mx-auto">
       <Countdown timeLeft={timeLeft} />
     </div>
+    
     </div>
   )
 }
